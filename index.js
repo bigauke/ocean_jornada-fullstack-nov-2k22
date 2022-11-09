f623cfc6be32dd7f7d8e34498c522fe8b3445dee
Daniel Linhares <danielinhares@gmail.com>
2022-11-08 21:24:38 -0300

    criado endpoint post /itens

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// sinalizamos que estamos usando JSON no body
+app.use(express.json());
+
 app.get('/', function(req, res){
     res.send("Hello World!!!!!!");
 });
@@ -17,6 +20,21 @@ const itens = ["Café Pelé", "Café Pilão", "Café Arábico"];
 app.get('/itens', function(req, res) {
     res.send(itens);
 });
+
+// Endpoint [POST] /itens CREATE
+app.post('/itens', function(req, res){
+    // console.log(req.body)
+
+    // Pegamos o nome enviado no body
+    const item = req.body.nome;
+
+    // inserimos o valor recebido na lista
+    itens.push(item);
+
+    res.send("item criado com sucesso!")
+
+    // res.send("Criar um item")
+}) ;
 
 app.listen(3000, function () {
     console.log("Servidor rodando em http://localhost:3000");
